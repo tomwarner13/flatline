4797e9d51591122b6054399f342731c96c765f9d
tom warner <tomwarner13@gmail.com>
2017-11-18 12:52:26 -0700

    create project

---
+++ Flatline/FlatlineCore/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FlatlineCore
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			if (args.Length == 0) 
+      {
+        ExecuteCommand ("help");
+			}
+      else
+      {
+        ExecuteCommand (args [0]); //todo: find out how to get all but the first value of an array
+      }
+		}
+
+		private static void ExecuteCommand(string command, params string[] args)
+		{
+			switch (command) 
+			{
+			case "build":
+				//build project
+				break;
+			case "rebuild":
+				//rebuild project
+				break;
+			case "help":
+				//show help
+				break;
+			default:
+				Console.WriteLine($"Command {command} not recognized. Type 'help' for help.");
+				break;
+			}
+		}
+	}
+}
