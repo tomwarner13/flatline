cfc3d93eba793117443ec5a8678ee15a606f9d87
tom warner <tomwarner13@gmail.com>
2017-12-26 18:35:23 -0700

    clean up project status

---
+++ FlatlineCore/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace FlatlineCore
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			if (args.Length == 0) 
+      {
+        ExecuteCommand ("help");
+			}
+      else
+      {
+        ExecuteCommand(args[0], args.Skip(1)); //todo: find out how to get all but the first value of an array
+      }
+		}
+
+    private static void ExecuteCommand(string command, IEnumerable<string> args = null)
+		{
+			switch (command) 
+			{
+  			case "build":        
+          Console.WriteLine ($"building project with args: {string.Join(" ", args)}");
+  				break;
+        case "rebuild":
+          Console.WriteLine ($"rebuilding project with args: {string.Join(" ", args)}");
+  				break;
+        case "help":
+          Console.WriteLine($"Heaven helps those who help themselves.");
+  				break;
+  			default:
+  				Console.WriteLine($"Command '{command}' not recognized. Type 'help' for help.");
+  				break;
+			}
+		}
+	}
+}
